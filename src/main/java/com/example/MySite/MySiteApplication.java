6f479cfb40c20e495fee3972140c11325655b253
Shaaban Ebrahim <shaaban.eltanany.ext@orange.com>
2019-11-03 10:54:08 +0200

    init project

---
+++ src/main/java/com/example/MySite/MySiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MySite/MySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
